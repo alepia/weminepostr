16cac82621790aadc513b742cc319ab6a922e6ca
alepia <alipi92@gmail.com>
2023-03-07 12:37:31 +0800

    HakunaMatata

--- postrequest.js
+++ postrequest.js
@@ -9,7 +9,7 @@ const data = {
     "https://1drv.ms/w/s!ApesyPfHqOIshrJ0kpwp6-yS_3BVHw?e=xOGSST",
   intro:
     "Hi, I’m Alejandro Piacquadio, a software developer with experience in web application development. I love learning new technologies and am constantly looking for new opportunities to improve my skills. I am also a chef but am currently looking for a change. I would love to work on your team and help you build something amazing.",
-  note: "I am a fast learner and I am always looking for new opportunities to improve my skills.",
+  note: "HakunaMatataSimbaBMW",
   code_url: "https://github.com/alepia/weminepostr",
 };
 
